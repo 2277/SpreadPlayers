d50eb5bfd52f699b51f5f3f58f4c25d0f0298109
2277 <38501234+2277@users.noreply.github.com>
2019-08-05 08:05:11 +0100

    Update Plugin.java

--- src/main/java/net/socialhangover/spreadplayers/Plugin.java
+++ src/main/java/net/socialhangover/spreadplayers/Plugin.java
@@ -1,10 +1,18 @@
 package net.socialhangover.spreadplayers;
 
+import lombok.Getter;
+import net.socialhangover.spreadplayers.listeners.PlayerListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
 
+    @Getter
+    private static Plugin instance = null;
+
     @Override
     public void onEnable() {
+        instance = this;
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
     }
 }
