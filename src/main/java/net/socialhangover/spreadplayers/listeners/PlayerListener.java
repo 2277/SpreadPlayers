b580031c72dff30b79cf5c9b23b0af88ed0c0999
2277 <38501234+2277@users.noreply.github.com>
2019-08-05 09:10:56 +0100

    Update PlayerListener.java

--- src/main/java/net/socialhangover/spreadplayers/listeners/PlayerListener.java
+++ src/main/java/net/socialhangover/spreadplayers/listeners/PlayerListener.java
@@ -1,14 +1,25 @@
 package net.socialhangover.spreadplayers.listeners;
 
+import lombok.RequiredArgsConstructor;
+import net.socialhangover.spreadplayers.Plugin;
+import net.socialhangover.spreadplayers.config.ConfigKeys;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+@RequiredArgsConstructor
 public class PlayerListener implements Listener {
+
+    private final Plugin plugin;
 
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
-
+        if (!event.getPlayer().hasPlayedBefore()) {
+            Bukkit.getServer()
+                    .dispatchCommand(Bukkit.getConsoleSender(), plugin.getConfiguration().get(ConfigKeys.SPREAD).getCommandString(event.getPlayer()));
+            event.getPlayer().setBedSpawnLocation(event.getPlayer().getLocation(), true);
+        }
     }
 
 }
