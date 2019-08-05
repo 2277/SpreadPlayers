057919b1c7c2198a14261cc7eb4f4df7b95bfded
2277 <38501234+2277@users.noreply.github.com>
2019-08-05 09:10:53 +0100

    Update Plugin.java

--- src/main/java/net/socialhangover/spreadplayers/Plugin.java
+++ src/main/java/net/socialhangover/spreadplayers/Plugin.java
@@ -1,18 +1,35 @@
 package net.socialhangover.spreadplayers;
 
 import lombok.Getter;
+import net.socialhangover.spreadplayers.config.Configuration;
 import net.socialhangover.spreadplayers.listeners.PlayerListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class Plugin extends JavaPlugin {
 
     @Getter
-    private static Plugin instance = null;
+    private static Plugin instance;
+
+    @Getter
+    private Configuration configuration;
 
     @Override
     public void onEnable() {
         instance = this;
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+        this.configuration = new Configuration(resolveConfig());
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
+    }
+
+    private File resolveConfig() {
+        File folder = getDataFolder();
+        File file = new File(folder, "config.yml");
+        if (!file.exists()) {
+            folder.mkdirs();
+            saveResource("config.yml", false);
+        }
+        return file;
     }
 }
