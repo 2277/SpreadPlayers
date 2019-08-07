233243c2146cf13bbb5e1beb752ed959df84a42b
2277 <38501234+2277@users.noreply.github.com>
2019-08-07 14:37:20 +0100

    Update SpreadCommand.java

--- src/main/java/net/socialhangover/spreadplayers/commands/SpreadCommand.java
+++ src/main/java/net/socialhangover/spreadplayers/commands/SpreadCommand.java
@@ -1,38 +1,45 @@
 package net.socialhangover.spreadplayers.commands;
 
-import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Subcommand;
-import lombok.RequiredArgsConstructor;
 import net.socialhangover.spreadplayers.SpreadPlugin;
 import net.socialhangover.spreadplayers.locale.message.Message;
+import net.socialhangover.spreadplayers.util.PlayerUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
-@CommandAlias("%rootcommand")
 public class SpreadCommand extends BaseCommand {
 
-    private final SpreadPlugin plugin;
+    public SpreadCommand(SpreadPlugin plugin) {
+        super(plugin);
+    }
 
-    @Subcommand("reload")
-    @CommandPermission("spread.reload")
     public void onReload(CommandSender sender) {
         plugin.getConfiguration().reload();
         sender.sendMessage(Message.RELOAD.asString(plugin.getLocaleManager()));
     }
 
-    @Subcommand("reset")
-    @CommandPermission("spread.reset")
-    public void onReset(CommandSender sender, OfflinePlayer player) {
+    public void onReset(CommandSender sender, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(Message.ERROR_MISSING_ARUGMENT.asString(plugin.getLocaleManager(), "/spread reset <player>"));
+            return;
+        }
+        OfflinePlayer player = PlayerUtils.getOfflinePlayer(args[1]);
+        if (player == null) {
+            sender.sendMessage(Message.ERROR_PLAYER_NOT_FOUND.asString(plugin.getLocaleManager(), args[1]));
+            return;
+        }
+
         File file = new File(plugin.getUserManager().getFolder(), player.getUniqueId() + ".yml");
         try {
             Files.deleteIfExists(file.toPath());
@@ -46,4 +53,34 @@ public class SpreadCommand extends BaseCommand {
         }
     }
 
+    @Override
+    public void onCommandExecute(CommandSender sender, Command command, String label, String[] args) {
+        switch (args[0].toLowerCase()) {
+            case "reload":
+                if (sender.hasPermission("spread.reload")) {
+                    onReload(sender);
+                }
+                break;
+            case "reset":
+                if (sender.hasPermission("spread.reset")) {
+                    onReset(sender, args);
+                }
+        }
+    }
+
+    @Override
+    public List<String> getTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return Arrays.asList("reload", "reset")
+                    .stream()
+                    .filter(v -> sender.hasPermission("spread." + v))
+                    .collect(Collectors.toList());
+        } if (args.length > 1 && args[0].equals("reset") && sender.hasPermission("spread.reset")) {
+            return Arrays.stream(Bukkit.getServer().getOfflinePlayers())
+                    .map(p -> p.getName().toLowerCase())
+                    .filter(v -> v.startsWith(args[2].toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
 }
