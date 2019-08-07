0935db3b2bae3b5f521a26f09f6fb5d59386c95b
2277 <38501234+2277@users.noreply.github.com>
2019-08-07 10:47:16 +0100

    Update PlayerListener.java

--- src/main/java/net/socialhangover/spreadplayers/listeners/PlayerListener.java
+++ src/main/java/net/socialhangover/spreadplayers/listeners/PlayerListener.java
@@ -3,10 +3,13 @@ package net.socialhangover.spreadplayers.listeners;
 import lombok.RequiredArgsConstructor;
 import net.socialhangover.spreadplayers.Plugin;
 import net.socialhangover.spreadplayers.config.ConfigKeys;
+import net.socialhangover.spreadplayers.storage.UserData;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 @RequiredArgsConstructor
 public class PlayerListener implements Listener {
@@ -15,10 +18,30 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
-        if (!event.getPlayer().hasPlayedBefore()) {
+        UserData userData = plugin.getUserManager().load(event.getPlayer());
+        if (userData != null && (!event.getPlayer().hasPlayedBefore() || userData.SPAWN_LOCATION.get() == null)) {
             Bukkit.getServer()
-                    .dispatchCommand(Bukkit.getConsoleSender(), plugin.getConfiguration().get(ConfigKeys.SPREAD).getCommandString(event.getPlayer()));
-            event.getPlayer().setBedSpawnLocation(event.getPlayer().getLocation(), true);
+                    .dispatchCommand(Bukkit.getConsoleSender(), plugin.getConfiguration()
+                            .get(ConfigKeys.SPREAD)
+                            .getCommandString(event.getPlayer()));
+
+            Location location = event.getPlayer().getLocation();
+            event.getPlayer().setBedSpawnLocation(location, true);
+            userData.SPAWN_LOCATION.set(location);
+            userData.save();
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawnEvent(PlayerRespawnEvent event) {
+        if (event.isBedSpawn()) {
+            return;
+        }
+        UserData userData = plugin.getUserManager().load(event.getPlayer());
+        if (userData == null) { return; }
+        Location location = userData.SPAWN_LOCATION.get();
+        if (location != null) {
+            event.setRespawnLocation(location);
         }
     }
 
