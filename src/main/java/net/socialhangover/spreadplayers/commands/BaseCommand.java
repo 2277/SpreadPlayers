bc3d15aa235360f72c7d6bfa362cf4a3e6934815
2277 <38501234+2277@users.noreply.github.com>
2019-08-07 14:37:08 +0100

    Update BaseCommand.java

--- src/main/java/net/socialhangover/spreadplayers/commands/BaseCommand.java
+++ src/main/java/net/socialhangover/spreadplayers/commands/BaseCommand.java
@@ -1,5 +1,6 @@
 package net.socialhangover.spreadplayers.commands;
 
+import lombok.RequiredArgsConstructor;
 import net.socialhangover.spreadplayers.SpreadPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -11,8 +12,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 public abstract class BaseCommand implements CommandExecutor, TabCompleter {
-    protected SpreadPlugin plugin;
+
+    protected final SpreadPlugin plugin;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        onCommandExecute(sender, command, label, args);
+        return true;
+    }
+
+    public abstract void onCommandExecute(CommandSender sender, Command command, String label, String[] args);
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
